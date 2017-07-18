00dc9aff8c8158b0791cef81bf3669c71db9a1f3
Marc King <mdkmba@gmail.com>
2017-07-18 13:09:03 -0400

    working on syntax_ changed to new char logic

--- src/AblayIxsay.java
+++ src/AblayIxsay.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
+/**
+ * Created by marcking2 on 7/17/17.
+ */
+
+import java.util.*;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-/**
- * Created by marcking2 on 7/16/17.
- */
-public class AblayIxsay {
+public class lab6 {
+
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         String words;   //for original input from user
@@ -13,109 +15,75 @@ public class AblayIxsay {
         String pigVersion;  //used to store result of manipulation from pigtrans method
         String blank = " ";
         int goodstr = 0;
+        boolean hasNum = false;
 
-        System.out.print("\n\n\nWelcome to Pig Latin Translator!");
+        System.out.print("\n\n\nWelcome to Pig Latin Translator!\n\n\n");
 
         //make code to verify text has been entered
 
-
-
-            do {
-                System.out.println("\n\nEnter a line to be translated (500 characters or less): ");
-                words = scnr.nextLine();
-                if (words.length() >= 500){   //must be less than 500 chars
-                    System.out.println("Too many characters.");
-                    goodstr = 0;
-                }
-                else if (words.charAt(0) == ' '){
-                    System.out.println("You can't start with a blank space.");  //must not start with blank space
-                    goodstr = 0;
-                }else{
-                    goodstr = 1;
-                }
-            }while (goodstr == 0);
-            words = "The way is 99% clear!";  //fixme remove this
-            wordscopy = words;
+        do {
+            System.out.println("\n\nEnter a line to be translated (500 characters or less): ");
+            words = scnr.nextLine();
+            if (words.length() >= 500) {   //must be less than 500 chars
+                System.out.println("Too many characters.");
+                goodstr = 0;
+            } else if (words.charAt(0) == ' ') {
+                System.out.println("You can't start with a blank space.");  //must not start with blank space
+                goodstr = 0;
+            } else {
+                goodstr = 1;
+            }
+        } while (goodstr == 0);
+        words = "The way is 99% clear";  //fixme remove this
+        wordscopy = words;
 
         pigVersion = pigtrans(wordscopy);   /*send string copy to pigtrans. */
         System.out.println(pigVersion);
     }
 
-    public static String pigtrans(String convertcopy){
+    public static String pigtrans(String convertcopy) {
         //^^^method to convert string copy to pig latin
-    String converted;
-        ArrayList<String> arrayList = new ArrayList<>();//list array for concatenating wrd2add strings as they are generated
+        String wordbk = "";
 
-    int count = convertcopy.length();   //counts the number of chars in the convert copy to set the size of the string
-    boolean hasNum = false;
-        String wrd2add = (""); //will add wrd2add.toLowerCase(); later
+        StringTokenizer word = new StringTokenizer(convertcopy);
 
-        for (int i = 0; i < count; i++){
-            char x = convertcopy.charAt(i);
-            if (x != ' '){
-                if (Character.isDigit(x)){
-                    hasNum = true;
+        while (word.hasMoreTokens()) {
+            wordbk = convWord(word.nextToken());
 
-                }
-                else if (!Character.isDigit(x)) {
-                    hasNum = false;
-                }
-                wrd2add += x;
-            }
-            if (hasNum){
-                return wrd2add;
-            }
+            //System.out.print(word.nextToken() + ' ');
 
-            else if (!hasNum){
-                wrd2add = convWord(wrd2add);
-                return wrd2add;
-            }
+        }
 
-                wrd2add = "";
+        return wordbk;
     }
 
+    public static String convWord(String wrd2add) {
 
-    }
+        // code by Monique West
+        String newPig = " ";
 
+        char v = wrd2add.charAt(0);
+        if (v == 'a' || v == 'e' || v == 'o' || v == 'i' || v == 'u') {
+            //System.out.println("Vowel: " + wrd2add);
+            //process Pig Latin with vowel
 
-    public static String convWord (String wrd2add) {
-        boolean make1stUpper = false;
-        boolean consonant = false;
-        String wrd2add2 = "";
+            newPig = wrd2add + "way";
+            //System.out.print("Your translated word is: " + newPig);
 
-        int count = wrd2add.length();
-
-        if (Character.isUpperCase(wrd2add.charAt(0))) {
-            make1stUpper = true;
-        }
-        String wrd2addB = wrd2add.toLowerCase();
-
-        for (int i = 0; i < count; i++) {
-            char f = wrd2addB.charAt(0);
-            char x = wrd2addB.charAt(i);
-            if (x != 'a' && x != 'e' && x != 'i' && x != 'o' && x != 'u') {
-                consonant = true;
-                wrd2add2 = wrd2addB.substring(1) + f;
-            } else {
-                wrd2add2 = wrd2addB + x;
-
-            }
-
-
-        }
-        if (make1stUpper) {
-            wrd2add2 = wrd2add2.substring(0, 1).toUpperCase() + wrd2add2.substring(1);
-        }
-        if (consonant) {
-            wrd2add2 = wrd2add + "ay";
         } else {
-            wrd2add2 = wrd2add + "way";
-        }
-        return wrd2add2;
+            System.out.println("Consonant: " + wrd2add);
+            for (int i = 0; i < wrd2add.length(); i++) {
+
+                char p = wrd2add.charAt(i);
+                if (p == 'a' || p == 'e' || p == 'o' || p == 'i' || p == 'u') {
+                    //process pig Latin with consonant
+                    newPig = wrd2add.substring(i);
+                    newPig += wrd2add.substring(0, i);
+
+                }newPig = newPig + "ay";
+
+            }
+
+        }return newPig;
     }
 }
-
-//char charArray[] = new char [count];    //creates array to the exact size of the string sent
-
-//    convertcopy.getChars(0, count, charArray, 0 );
-//^^^moves (count) number of chars in the string to the array in sequential order
